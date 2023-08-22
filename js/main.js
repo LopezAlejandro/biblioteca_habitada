3a0bebd166ec733c43ed5be9645b1008a5e23d07
Alejandro Lopez <lopalejandro@gmail.com>
2023-08-22 14:29:42 -0300

    Creacion del main.js

---
+++ js/main.js
@@ -0,0 +1,178 @@
+function escribir(pagina) {
+    node = document.getElementById("main");
+    node.innerHTML = "";
+    switch (pagina) {
+        case 1:
+            text = `<div class="main-flex-container">
+        <div class="row">
+          <div class="col-2"></div>
+          <div class="col-8">
+
+            <figure class="text-end">
+              <blockquote class="blockquote">
+                <p class="preludio">“El secreto de todo buen bibliotecario consiste en no leer
+                  los libros que tiene a su cargo, exceptuando el título y el
+                  índice. ¡El que mete las narices en un libro está perdido!...
+                  Nunca llegará a tener la perspectiva del conjunto.“</p>
+              </blockquote>
+              <figcaption class="blockquote-footer">
+                Robert Musil, <cite title="Source Title">Der Mann ohne
+                  Eigenschaften, Berlin, 1930.</cite>
+              </figcaption>
+            </figure>
+
+            <figure class="text-end">
+              <blockquote class="blockquote">
+                <p class="preludio">“El hombre se comporta como si él fuera el señor y el
+                  forjador del lenguaje, cuando en realidad es éste el que es y
+                  ha sido siempre el señor del hombre. La inversión de esta
+                  relación llevada a cabo por el hombre en la era moderna, de
+                  esta relación de dominio, es lo que empuja a la esencia de
+                  aquel a lo no hogareño, a no habitar…”</p>
+              </blockquote>
+              <figcaption class="blockquote-footer">
+                Martín Heidegger, <cite title="Source Title">Construir, habitar,
+                  pensar. Darmstadt, 1951.</cite>
+              </figcaption>
+            </figure>
+            <br>
+
+            <div class="two-columns">
+              <p class="preludio">
+                <strong>La Biblioteca habitada</strong> es el nombre del
+                proyecto que hemos encarado desde el equipo curatorial en el
+                2021/2022.<br />
+                El espíritu que enuncia Robert Musil, imaginando a un
+                bibliotecario abnegado que trabaja en la Biblioteca Imperial de
+                Viena y que conoce uno por uno todos los títulos de su
+                gigantesco acervo, nos anima… Sabíamos que si metíamos las
+                narices en todo estábamos perdidos, queríamos no abarcarlo y si
+                de alguna forma construir una perspectiva del conjunto.
+                Comprendimos, luego del año y medio transcurrido, que tener esa
+                perspectiva de algunas de las historias que animan a la
+                biblioteca de la FADU UBA, no todas, nos permitiría habitarla
+                desde otro lugar.<br />
+                Muchas de estas historias son conocidas, otras no tanto y fueron
+                apareciendo a medida que el trabajo avanzaba y a veces también
+                retrocedía…<br />
+                Heidegger nos ayudó con su famoso ensayo
+                <strong>Construir, Habitar, Pensar</strong> al definir al
+                habitar no como un problema perteneciente al campo de la
+                arquitectura y la construcción, sino como un problema del
+                lenguaje. La relación de la inversión de dominio entre hombre y
+                lenguaje en la época moderna conlleva a una relación no
+                hogareña, a un no habitar, eso plantea Heidegger en su
+                escrito.<br />
+                Apareció en el proceso de las historias un momento liminal, en
+                el sentido de lo extraordinario:
+                <strong>La Biblioteca imaginaria, antiguos libros de la FADU</strong>
+                editado por EUDEBA en 1998.<br />Fue entonces que decidimos
+                convocar a Enrique Longinotti con su testimonio, recreando aquel
+                tiempo… De alguna manera Enrique nos trajo una certera
+                <em>ekprhasis</em> —a pesar de no pretenderlo— de nuestra
+                biblioteca, en su descripción pormenorizada de aquel momento…
+              </p>
+            </div>
+          </div>
+          <div class="col-2"></div>
+        </div>
+      </div>`;
+            break;
+        case 2:
+            text = `<div class="main-flex-container">
+            <div class="row">
+                <div class="col-2"></div>
+                <div class="col-8">
+
+                    <div class="two-columns">
+                        <p class="preludio">
+                            Volver a transitar los libros antiguos que forman parte de nuestro acervo “como una
+                            combinación de leer y mirar” es una forma de volver a habitarlos… <br />
+                            Roberto Doberti en el prólogo de <strong>La Biblioteca Imaginaria</strong> vuelve sobre esta
+                            relación entre
+                            habitar y lenguaje en términos de habla y escritura, “que se acompañan en largas,
+                            interminables, permanentes, danzas y contradanzas”.<br>
+
+                            Danzas y contradanzas del proyecto es la matriz disciplinar de nuestra FADU. <br /><br />
+                            Quisimos, como hicimos en el 2020, contar la historia de un libro con aura de nuestra
+                            colección: <strong>La interacción del color de Josef Albers</strong>, hablar de otros libros…<br>
+                            Imposible hablar de todos, por ello elegimos uno emblemático. <strong>El Fletcher de la
+                                biblioteca</strong> que certeramente Horacio Caride Batrons completó en su definición como
+                            <strong>Un libro y un siglo</strong> y esta cita que prologa su texto:<br></p>
+                        <blockquote class="blockquote">
+                            <p class="preludio">
+                                “Este libro ha sido comprado durante 100 años por todos los estudiantes de arquitectura
+                                que
+                                han podido reunir los chelines. No tiene rivales en su maravilloso análisis de las
+                                grandes
+                                obras del pasado europeo. Si quieres comparar San Pedro con el Panteón o Santa Sofía,
+                                recurres a Banister Fletcher para obtener dibujos medidos y una descripción sucinta.”
+                            </p>
+                        </blockquote>
+                        <p class="preludio">
+                            Caride ilustra profusamente con su ensayo la historia de un libro que ha formado a legiones
+                            de arquitectos en la historia de la arquitectura de nuestra biblioteca. Con inasibles
+                            categorías, con árboles de dudosa genealogía, pero maravillosos dibujos, descubrimos que en
+                            la entonces biblioteca de la FAU la primer edición registrada —inexistente hoy en día
+                            físicamente y dada de baja por su deterioro— fue la decimotercera de 1946, la del jubileo de
+                            los cincuenta años del libro.<br>
+
+                            El proyecto convocó a un cuidado de los materiales que iban apareciendo.<br>
+                            Heidegger plantea al habitar como un cuidado.<br></p>
+                            <blockquote><p class="preludio">
+
+                            “El modo en que los mortales son la tierra, bajo un cielo, ante los divinos, es el modo en
+                            que acaece este cuádruple cuidado. Eso es el habitar poético.”</p></blockquote>
+                            <p class="preludio">
+                            Un hombre situado y consciente de su finitud —por ello mortal— bajo la bóveda de un cielo,
+                            arraigado a la tierra, —y por tanto situado, no genéricamente en un lugar— en relación a
+                            aquello que no es dominado y controlado racionalmente por su voluntad y que Heidegger
+                            denomina: los dioses, los divinos. Metáfora preclara de toda existencia trascendente.
+                            Constituye como tal, una certera definición de ese lenguaje no utilitario, al que
+                            denominamos poesía.<br>
+
+                            El habitar como un cuádruple cuidado que custodia el lenguaje poético.
+
+                        </p>
+                    </div>
+                </div>
+                <div class="col-2"></div>
+            </div>
+        </div>`;
+            break;
+        case 3:
+            text = ` <div class="main-flex-container">
+            <div class="row">
+                <div class="col-2"></div>
+                <div class="col-8">
+
+                    <div class="two-columns">
+                        <p class="preludio">
+                            Cuando el hombre antepone sus maquinaciones, especulaciones, razonamientos, sobre la tierra, sobre el cielo, avasallando lo sagrado de cada existencia, no hace más que alterar e invertir la relación de dominio, tornando al mundo en no hogareño, en no habitable.<br><br> 
+Más allá de las resonancias que el habitar poético nos trae en la contemporaneidad de la pospandemia, podemos preguntar ¿cuánto de todo esto tiene que ver con esa falta de cuidado del lenguaje de los hombres, con esa inversión de dominio que hemos  ejercido sin más frente al lenguaje, a los otros, a la naturaleza, al mundo en general? <br><br>
+<strong>La Biblioteca habitada</strong> está dedicada como proyecto a los estudiantes, a los ingresantes a la FADU en estos últimos dos años que no han conocido el edificio de Ciudad Universitaria, ni a sus compañeros, ni a sus profesores, ni a la biblioteca.  
+Pertinente en esta pausa forzada en donde todo se redujo a lo no habitable de una pantalla de teléfono o de computadora…<br>  
+No habitamos la facultad, no habitamos más sus espacios, y entre ellos la biblioteca.<br> 
+Implicar el proyecto a lo que significa lo inverso, <strong>La Biblioteca habitada</strong>, algunas historias, sus espacios, sus libros y protagonistas en el tiempo, directores, el personal de la biblioteca —con la noble tarea de los bibliotecarios, los curadores de la colección, los que la preservan y cuidan en nuestro taller de restauración, los que prestan los libros a los estudiantes, investigadores docentes; aconsejado una u otra edición, en definitiva los que habitan y habitaron algunas de estas historias, los libros y estos espacios…<br><br> 
+La estructura del proyecto <strong>La Biblioteca habitada</strong> se impuso tripartita: Los espacios, Los libros, Los directores. Condensando en esta esencia multiplicidades, no con el ánimo de construir una totalidad… sino el testimonio inicial de un proyecto abierto para las futuras generaciones que nos sucedan y nos lean.
+                        </p>
+                    </div>
+                </div>
+                <div class="col-2"></div>
+            </div>
+        </div>`;
+            break;
+        case 4:
+            break;
+        case 5:
+            break;
+        case 6:
+            break;
+    
+        default:
+            break;
+    }
+
+    node.innerHTML = text;
+ 
+}
